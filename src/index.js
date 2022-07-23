ba5b71730b325ee6ea778b1c4fddfcf57a02612f
TonyaSmetanina <53794265+TonyaSmetanina@users.noreply.github.com>
2022-07-23 21:56:20 +0300

    update the time

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
+function formatDate (timestamp) {
+    //calculate the date
+
+    let date = new Date (timestamp);
+    let hour = date.getHours();
+    if (hour < 10) {
+        hour = `0${hour}`;
+    }
+    let minutes = date.getMinutes ();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [
+    "Sunday",
+    "Monday", 
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday" ];
+    let day = days [date.getDay ()];
+    return `${day} ${hour}:${minutes}`;
+}
+
+
 function displayTemperature (response) {
-    console.log (response.data);
 
     let descriptionElement = document.querySelector ("#description");
     descriptionElement.innerHTML = response.data.weather[0].description;
@@ -16,6 +40,9 @@ function displayTemperature (response) {
 
     let windElement = document.querySelector ("#wind");
     windElement.innerHTML = Math.round (response.data.wind.speed);
+    
+    let dateElement = document.querySelector ("#date");
+    dateElement.innerHTML = formatDate (response.data.dt * 1000);
 }
 
 let apiKey = "2385e38ab7a755a47b225394ab5f5fc0";
