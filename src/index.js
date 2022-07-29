f36a8cf9bb750d49b0172bbe5bf8aefbcfd1d6ce
TonyaSmetanina <53794265+TonyaSmetanina@users.noreply.github.com>
2022-07-29 13:29:51 +0300

    Added Api for display forecast

--- src/index.js
+++ src/index.js
@@ -22,6 +22,47 @@ function formatDate (timestamp) {
     return `${day} ${hour}:${minutes}`;
 }
 
+function displayForecast (response) {
+    console.log (response.data.daily);
+    let forecastElement = document.querySelector ("#forecast");
+
+    let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+
+    let forecastHTML = `<div class = "row">`;
+    days.forEach(function(day) {
+        forecastHTML = forecastHTML + `
+        <div class="col-2">
+            <div class="weather-forecast-date">
+                ${day}
+            </div>
+            
+            <img src="https://openweathermap.org/img/wn/04n@2x.png" 
+            alt=""
+            width="36"
+            /> 
+            <div class="weather-forecast-temperature">
+               <span class="weather-forecast-temperature-max">
+                28 </span> 
+                <span class="weather-forecast-temperature-min">
+                    19 </span>
+            </div>
+        </div>`;
+    
+    })
+    
+    forecastHTML = forecastHTML + `</div>`
+
+    forecastElement.innerHTML = forecastHTML; 
+
+}
+
+function getForecast (coordinates) {
+    console.log (coordinates);
+    let apiKey = "2385e38ab7a755a47b225394ab5f5fc0";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature (response) {
 
@@ -51,6 +92,7 @@ function displayTemperature (response) {
 
     celciumTemp = response.data.main.temp; 
 
+    getForecast (response.data.coord); 
 }
 
 function search (city) {
